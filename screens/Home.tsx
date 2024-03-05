916d2163b51638c008d2dc207b940f347061560d
smarttechfire <educationfire552@gmail.com>
2024-03-05 12:24:26 +0530

    Running our first query

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import { Category, Transaction } from '../type';
 import { useSQLiteContext } from 'expo-sqlite/next';
@@ -7,6 +7,18 @@ export default function Home() {
     const [categories,setCategories] = useState<Category[]>([]);
     const [transactions,setTransactions] = useState<Transaction[]>([]);
     const db = useSQLiteContext();
+
+    useEffect(() =>{
+        db.withTransactionAsync(async () =>{
+            await getData();
+        })
+    },[db])
+
+    async function getData() {
+        const result = await db.getAllAsync<Transaction>(`SELECT * FROM Transactions ORDER BY date DESC;`);
+        setTransactions(result);
+        // console.log(result);
+    }
   return (
     <View>
       <Text>Home Tour</Text>
