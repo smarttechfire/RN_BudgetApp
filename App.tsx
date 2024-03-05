5bdf9ebeeb691b86aa6b367537afb1b97ccbcde0
smarttechfire <educationfire552@gmail.com>
2024-03-05 11:50:16 +0530

    Installing dep , Importing existing SQLiteDB

--- App.tsx
+++ App.tsx
@@ -1,12 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import * as FileSystem from "expo-file-system";
+import { Asset } from "expo-asset";
+import { Suspense, useEffect, useState } from "react";
+import { SQLiteProvider } from "expo-sqlite/next";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from './screens/Home';
 
+const Stack = createNativeStackNavigator();
+const loadDatabse = async () => {
+  const dbName = "mySQLiteDB.db";
+  const dbAsset = require("./assets/mySQLiteDB.db");
+  const dbUri = Asset.fromModule(dbAsset).uri;
+  const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`;
+  const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
+  if (!fileInfo.exists) {
+    await FileSystem.makeDirectoryAsync(
+      `${FileSystem.documentDirectory}SQLite`,
+      { intermediates: true }
+    );
+    await FileSystem.downloadAsync(dbUri, dbFilePath);
+  }
+};
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const [dbloaded, setDbLoaded] = useState<boolean>(false);
+  useEffect(() => {
+    loadDatabse()
+      .then(() => setDbLoaded(true))
+      .catch((e) => console.error(e));
+  }, []);
+  if (!dbloaded) return (
+    <View style={{ flex: 1 }}>
+      <ActivityIndicator size={"large"} />
+      <Text>Loading Database...</Text>
     </View>
+  )
+  return (
+    <NavigationContainer>
+      <Suspense>
+        <SQLiteProvider databaseName="mySQLiteDB.db" useSuspense>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home} />
+          </Stack.Navigator>
+        </SQLiteProvider>
+      </Suspense>
+    </NavigationContainer>
   );
 }
 
